85635edee6e8b775a954b58587d596b2711fa743
Kyu Lee <kwooler@gmail.com>
2023-05-12 21:00:33 -0700

    CS7L1002

--- main_test.py
+++ main_test.py
@@ -19,7 +19,9 @@ def test_main_1():
 
     r = main.Rectangle(10, 20)
     print(f'Height: {r.height} \t Width: {r.width}')
-    assert True
+
+    assert r.height == 10
+    assert r.width == 20
 
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
@@ -29,3 +31,25 @@ def test_main_1():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
+    # sys.stdin = io.StringIO(datastr)
+
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    r = main.Rectangle(10, 20)
+    print(f'Height: {r.height} \t Width: {r.width}')
+
+    r._height = 100
+    r._width = 200
+    print(f'Height: {r.height} \t Width: {r.width}')
+    assert r._height == 100
+    assert r.width == 200
+    assert r.get_height() == 100
