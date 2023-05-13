f4164c88da463fdb19f5ae55cc5bef76d25e0e46
Kyuwoong Lee <kwooler@gmail.com>
2023-05-12 19:51:41 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+class Rectangle:
+#########################################
+# Code your program here
+# Define the class Rectangle
+#########################################
+
+
+def main():
+
+    r = Rectangle(10, 20)
+    print(f'Height: {r.height} \t Width: {r.width}')
+
+
+if __name__ == '__main__':
+    main()
